6ba31f723e157894e7b17f07f4c230e631cf20f0
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-07-20 05:52:12 -0400

    added button function

--- src/pages/SignUpPage.spec.ts
+++ src/pages/SignUpPage.spec.ts
@@ -1,10 +1,77 @@
 import SignUpPage from './SignUpPage.svelte'
 import { render, screen } from '@testing-library/svelte'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 
+describe( 'Sign Up Page', () => {
+    describe( 'Layout', () => {
+        it( 'has Sign Up Header', () => {
+            render( SignUpPage )
+            const header = screen.getByRole( 'heading', { name: 'Sign Up' } )
+            expect( header ).toBeInTheDocument()
+        } )
 
-it( 'has Sign Up Header', () => {
-    render( SignUpPage )
-    const header = screen.getByRole( 'heading', { name: 'Sign Up' } )
-    expect( header ).toBeInTheDocument()
+        it( 'has username input', () => {
+            render( SignUpPage )
+            const input: HTMLInputElement = screen.getByLabelText( 'Username' )
+            expect( input ).toBeInTheDocument()
+        } )
+
+        it( 'has email input', () => {
+            render( SignUpPage )
+            const input: HTMLInputElement = screen.getByLabelText( 'E-mail' )
+            expect( input ).toBeInTheDocument()
+        } )
+
+        it( 'has password input', () => {
+            render( SignUpPage )
+            const input: HTMLInputElement = screen.getByLabelText( 'Password' )
+            expect( input ).toBeInTheDocument()
+        } )
+
+        it( 'has password type for password input', () => {
+            render( SignUpPage )
+            const input: HTMLInputElement = screen.getByLabelText( 'Password' )
+            expect( input.type ).toBe( 'password' )
+        } )
+
+        it( 'has password repeat input', () => {
+            render( SignUpPage )
+            const input: HTMLInputElement = screen.getByLabelText( 'Repeat Password' )
+            expect( input ).toBeInTheDocument()
+        } )
+
+        it( 'has password type for password repeat input', () => {
+            render( SignUpPage )
+            const input: HTMLInputElement = screen.getByLabelText( 'Repeat Password' )
+            expect( input.type ).toBe( 'password' )
+        } )
+
+        it( 'has Sign Up button', () => {
+            render( SignUpPage )
+            const header = screen.getByRole( 'button', { name: 'Sign Up' } )
+            expect( header ).toBeInTheDocument()
+        } )
+
+        it( 'has Sign Up button disabled', () => {
+            render( SignUpPage )
+            const button = screen.getByRole( 'button', { name: 'Sign Up' } )
+            expect( button ).toBeDisabled()
+        } )
+    } )
+
+    describe( 'Interactions', () => {
+        it( 'enables the button when the password and password repeat fields have same value', async () => {
+            render( SignUpPage )
+            const passwordInput: HTMLInputElement = screen.getByLabelText( 'Password' )
+            const passwordRepeatInput: HTMLInputElement = screen.getByLabelText( 'Repeat Password' )
+
+            await userEvent.type( passwordInput, 'P4ssword' )
+            await userEvent.type( passwordRepeatInput, 'P4ssword' )
+            const button = screen.getByRole( 'button', { name: 'Sign Up' } )
+            expect( button ).toBeEnabled()
+
+        } )
+    } )
+
 } )
--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -1,1 +1,34 @@
+<script lang="ts">
+    let disabled = true
+    let password: string
+    let passwordRepeat: string
+
+    const onChangePassword = (event: any) => {
+        password = event.target.value
+        refreshDisabled()
+    }
+
+    const onChangePasswordRepeat = (event: any) => {
+        passwordRepeat = event.target.value
+        refreshDisabled()
+    }
+
+    const refreshDisabled = () => {
+        disabled = password === passwordRepeat
+    }
+</script>
+
 <h1>Sign Up</h1>
+<label for="username">Username</label>
+<input id="username" />
+
+<label for="e-mail">E-mail</label>
+<input id="e-mail" />
+
+<label for="password">Password</label>
+<input id="password" type="password" on:input={onChangePassword} />
+
+<label for="password-repeat">Repeat Password</label>
+<input id="password-repeat" type="password" on:input={onChangePasswordRepeat} />
+
+<button {disabled}>Sign Up</button>
