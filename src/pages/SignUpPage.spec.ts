b3c20fee796388b4c87e63f9f209c5eb90918451
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-07-20 06:35:43 -0400

    Added buttonclick type, mock event for button submit

--- src/pages/SignUpPage.spec.ts
+++ src/pages/SignUpPage.spec.ts
@@ -2,6 +2,7 @@ import SignUpPage from './SignUpPage.svelte'
 import { render, screen } from '@testing-library/svelte'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
+import axios from 'axios'
 
 describe( 'Sign Up Page', () => {
     describe( 'Layout', () => {
@@ -72,6 +73,35 @@ describe( 'Sign Up Page', () => {
             expect( button ).toBeEnabled()
 
         } )
+
+        it( 'sends username, email, and password to backend after clicking button', async () => {
+            render( SignUpPage )
+            const usernameInput: HTMLInputElement = screen.getByLabelText( 'Username' )
+            const emailInput: HTMLInputElement = screen.getByLabelText( 'E-mail' )
+            const passwordInput: HTMLInputElement = screen.getByLabelText( 'Password' )
+            const passwordRepeatInput: HTMLInputElement = screen.getByLabelText( 'Repeat Password' )
+
+            await userEvent.type( usernameInput, 'user1' )
+            await userEvent.type( emailInput, 'user1@email.com' )
+            await userEvent.type( passwordInput, 'P4ssword' )
+            await userEvent.type( passwordRepeatInput, 'P4ssword' )
+            const button = screen.getByRole( 'button', { name: 'Sign Up' } )
+
+            const mockFn = jest.fn()
+            axios.post = mockFn
+
+            await userEvent.click( button )
+            const firstCall = mockFn.mock.calls[ 0 ]
+
+            const body = firstCall[ 1 ]
+            expect( body ).toEqual( {
+                username: 'user1',
+                email: 'user1@email.com',
+                password: 'P4ssword'
+            } )
+
+        } )
     } )
+
 
 } )
--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -1,24 +1,39 @@
 <script lang="ts">
+    import axios from "axios"
     let disabled = true
-    let password: string
-    let passwordRepeat: string
+    let password: string,
+        passwordRepeat: string,
+        username: string,
+        email: string
 
     // Reactive
     $: disabled =
         password && passwordRepeat ? password !== passwordRepeat : true
+
+    type ButtonClick = MouseEvent & {
+        currentTarget: EventTarget & HTMLButtonElement
+    }
+
+    const submit = (event: ButtonClick) => {
+        event.preventDefault()
+        axios.post("/api/1.0/users", { username, email, password })
+    }
 </script>
 
 <h1>Sign Up</h1>
-<label for="username">Username</label>
-<input id="username" />
 
-<label for="e-mail">E-mail</label>
-<input id="e-mail" />
+<form>
+    <label for="username">Username</label>
+    <input id="username" bind:value={username} />
 
-<label for="password">Password</label>
-<input id="password" type="password" bind:value={password} />
+    <label for="e-mail">E-mail</label>
+    <input id="e-mail" bind:value={email} />
 
-<label for="password-repeat">Repeat Password</label>
-<input id="password-repeat" type="password" bind:value={passwordRepeat} />
+    <label for="password">Password</label>
+    <input id="password" type="password" bind:value={password} />
 
-<button {disabled}>Sign Up</button>
+    <label for="password-repeat">Repeat Password</label>
+    <input id="password-repeat" type="password" bind:value={passwordRepeat} />
+
+    <button {disabled} on:click={submit}>Sign Up</button>
+</form>
