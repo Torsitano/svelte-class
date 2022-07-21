92955b12aa690c5c5f4592dcddab0c0da48255a7
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-07-21 08:21:18 -0400

    added styling to form

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-  import SignUpPage from "./pages/SignUpPage.svelte"
+    import SignUpPage from "./pages/SignUpPage.svelte"
 </script>
 
-<SignUpPage />
+<div class="container">
+    <SignUpPage />
+</div>
